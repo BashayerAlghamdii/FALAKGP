68c17efb0047ef801d4be7d4418eb6fbb58744b3
Bashayer <bashayeralgghamdi@gmail.com>
2023-02-11 19:31:55 +0300

    committ

--- app/src/main/java/com/example/falaksign/aboutus.java
+++ app/src/main/java/com/example/falaksign/aboutus.java
@@ -10,8 +10,5 @@ public class aboutus extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutus);
-
-
-        ///Hi hhhhhhhhhhh
     }
 }
