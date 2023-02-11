b20d9f621507586fe46ce9566b18b38bb1383cd5
HAIFASPACE99 <122253319+HAIFASPACE99@users.noreply.github.com>
2023-02-12 00:44:28 +0300

    commmit

--- app/src/main/java/com/example/falaksign/events.java
+++ app/src/main/java/com/example/falaksign/events.java
@@ -6,28 +6,79 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class events extends AppCompatActivity {
+
+
+
+    ImageView ssc ;
+    ImageView nasa ;
+    ImageView spacex ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
-        View home1 = findViewById(R.id.homeButton7);
-        home1.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                openhome();
+        ssc = findViewById(R.id.ssc);
+        nasa = findViewById(R.id.nasa);
+        spacex = findViewById(R.id.spacex);
+        ssc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://saudispace.gov.sa/news/ju2322/");
+            }
+
+
+        });
+
+        nasa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.nasa.gov/");
+            }
+
+
+        });
+
+
+
+
+
+        spacex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://www.spacex.com/");
             }
         });
 
-    }
-    public void openhome() {
-        Intent intent = new Intent(this, Homepage.class);
-        startActivity(intent);
+
+
+
+
+      //  View home1 = findViewById(R.id.homeButton7);
+       // home1.setOnClickListener(new View.OnClickListener() {
+          //  public void onClick(View v) {
+           //     openhome();
+          //  }
+       // });
 
     }
+   // public void openhome() {
+     //   Intent intent = new Intent(this, Homepage.class);
+        //startActivity(intent);
+
+    //}
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
+
+
 }
 //homeButton7
